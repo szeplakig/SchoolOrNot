bcf745bb30dd549ea53df214731ed1def6e4ea8e
Procok <szeplakig91@gmail.com>
2021-02-07 18:36:39 +0100

    Updated timetable for 6th semester

--- index.js
+++ index.js
@@ -10,34 +10,24 @@ const days = [
 
 const timeTable = {
   Monday: {
-    School: [{ from: "12:00:00", to: "16:00:00" }],
-    Work: [{ from: "08:00:00", to: "12:00:00" }],
+    School: [{ from: "08:00:00", to: "16:00:00" }],
+    Work: [{ from: "12:00:00", to: "16:00:00", tentative: true }],
   },
   Tuesday: {
-    School: [
-      { from: "08:00:00", to: "10:00:00" },
-      { from: "13:00:00", to: "15:00:00" },
-    ],
-    Work: [
-      { from: "10:00:00", to: "13:00:00" },
-      { from: "13:00:00", to: "15:00:00", tentative: true },
-      { from: "15:00:00", to: "17:00:00" },
-    ],
+    School: [],
+    Work: [{ from: "08:00:00", to: "16:00:00" }],
   },
   Wednesday: {
-    School: [{ from: "08:00:00", to: "19:15:00" }],
-    Work: [{ from: "14:00:00", to: "16:00:00", tentative: true }],
+    School: [],
+    Work: [{ from: "08:00:00", to: "16:00:00" }],
   },
   Thursday: {
-    School: [{ from: "12:00:00", to: "16:00:00" }],
-    Work: [
-      { from: "08:00:00", to: "12:00:00" },
-      { from: "12:00:00", to: "14:00:00", tentative: true },
-    ],
+    School: [],
+    Work: [{ from: "08:00:00", to: "16:00:00" }],
   },
   Friday: {
-    School: [{ from: "08:00:00", to: "12:00:00" }],
-    Work: [{ from: "12:00:00", to: "17:00:00" }],
+    School: [{ from: "08:00:00", to: "16:00:00" }],
+    Work: [{ from: "12:00:00", to: "16:00:00", tentative: true }],
   },
   Saturday: {
     School: [],
@@ -69,29 +59,33 @@ function draw() {
 
   const time = document.getElementById("time");
   time.innerHTML = `<h1>${now.format("Do MMMM YYYY, HH:mm:ss")}</h1>`;
-  
+
   const seenTypes = [];
   const info = document.getElementById("info");
   const text = activities
-  .filter((act) => {
-    if (act.to.diff(now) <= 0) return false;
-    let seen = seenTypes.filter((t) => t == act.type).length < 2;
-    if (seen) seenTypes.push(act.type);
-    return seen;
-  })
-  .map((act) => {
-    if (now.isBetween(act.from, act.to)) {
-      return `<h3>${act.tentative ? "<u>(Tentative)</u> " : ""}Doing '${
-        act.type
-      }' right now for ${act.to.fromNow(true)}!</h3><h5>${act.from.calendar()} to ${act.to.calendar()}</h5>`;
-    } else {
-      return `<h4>${act.tentative ? "<u>(Tentative)</u> " : ""}'${
-        act.type
-      }' ${now.to(act.from)}, ${act.from.calendar()}</h4><h5>${act.from.calendar()} to ${act.to.calendar()}</h5>`;
-    }
-  })
-  .join("<br>")
-  if(info.innerHTML != text){
+    .filter((act) => {
+      if (act.to.diff(now) <= 0) return false;
+      let seen = seenTypes.filter((t) => t == act.type).length < 2;
+      if (seen) seenTypes.push(act.type);
+      return seen;
+    })
+    .map((act) => {
+      if (now.isBetween(act.from, act.to)) {
+        return `<h3>${act.tentative ? "<u>(Tentative)</u> " : ""}Doing '${
+          act.type
+        }' right now for ${act.to.fromNow(
+          true
+        )}!</h3><h5>${act.from.calendar()} to ${act.to.calendar()}</h5>`;
+      } else {
+        return `<h4>${act.tentative ? "<u>(Tentative)</u> " : ""}'${
+          act.type
+        }' ${now.to(
+          act.from
+        )}, ${act.from.calendar()}</h4><h5>${act.from.calendar()} to ${act.to.calendar()}</h5>`;
+      }
+    })
+    .join("<br>");
+  if (info.innerHTML != text) {
     info.innerHTML = text;
   }
 }
